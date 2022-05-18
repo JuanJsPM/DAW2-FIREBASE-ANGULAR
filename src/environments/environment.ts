4038878a306f8791b59d381a12c6a1e28bf49ae4
Juan <JuanJsPM@hotmail.com>
2022-05-18 23:37:32 +0100

    Initial commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyA8902sWL1OXtnYBn27RYqPw6KnYtEcqhg",
+    authDomain: "daw2-d69fa.firebaseapp.com",
+    projectId: "daw2-d69fa",
+    storageBucket: "daw2-d69fa.appspot.com",
+    messagingSenderId: "680299547102",
+    appId: "1:680299547102:web:de97da5a7c10eca7f3f54f",
+    measurementId: "G-0MYJL0VE0M"
+  
+  }
+};
